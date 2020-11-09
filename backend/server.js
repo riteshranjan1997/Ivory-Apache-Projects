4ff12ed5392cb9de068b10455b232e290983d796
Sainihal <sainihal13@gmail.com>
2020-11-09 16:02:53 +0530

    Google Auth

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const dotenv = require("dotenv")
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+dotenv.config()
 
 mongoose.connect(
-  "mongodb://localhost/auth",
+  process.env.ATLAS_URI,
   {
     useUnifiedTopology: true,
     useNewUrlParser: true,
