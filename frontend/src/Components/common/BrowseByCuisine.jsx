13695d035faad7f10b415fe82cb6a1971f3bda05
Ritesh Ranjan <riteshranjan1997@gmail.com>
2020-11-12 14:50:01 +0530

    update

--- frontend/src/Components/common/BrowseByCuisine.jsx
+++ frontend/src/Components/common/BrowseByCuisine.jsx
@@ -25,10 +25,6 @@ export default function BrowseByCuisine() {
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
-<<<<<<< HEAD
-=======
-
->>>>>>> aa54deb2eea1f6cd34a6183b0a9ece2a146f0b88
   return (
      <div className="container"> 
         <div className={classes.root}>
@@ -49,7 +45,6 @@ export default function BrowseByCuisine() {
                     <div>all restaurents</div>
                   </div>
                 }  {...a11yProps(0)} style={{textTransform:"capitalize"}} />
-<<<<<<< HEAD
                 <Tab label={<div>
                     <div><img src="https://media-cdn.grubhub.com/d_search:browse-images:default.jpg/d_search:browse-images:default.jpg/dpr_auto,c_fill,w_124,h_124,f_auto,q_auto,g_auto/search/browse-images/lunch-specials-v4.jpg" alt="JPEG" style={{height:"100px",width:"100px",borderRadius:"50%",objectFit:"cover"}}/></div>
                     <div>deserts</div>
@@ -83,41 +78,6 @@ export default function BrowseByCuisine() {
                     <div>Lunch Specials</div>
                 </div>} {...a11yProps(8)} style={{textTransform:"capitalize"}} />
                 <Tab label={<div>
-=======
-                <Tab label={<div>
-                    <div><img src="https://media-cdn.grubhub.com/d_search:browse-images:default.jpg/d_search:browse-images:default.jpg/dpr_auto,c_fill,w_124,h_124,f_auto,q_auto,g_auto/search/browse-images/lunch-specials-v4.jpg" alt="JPEG" style={{height:"100px",width:"100px",borderRadius:"50%",objectFit:"cover"}}/></div>
-                    <div>deserts</div>
-                </div>}  {...a11yProps(1)} style={{textTransform:"capitalize"}} />
-                <Tab  label={<div>
-                    <div><img src="https://media-cdn.grubhub.com/d_search:browse-images:default.jpg/d_search:browse-images:default.jpg/dpr_auto,c_fill,w_124,h_124,f_auto,q_auto,g_auto/search/browse-images/lunch-specials-v4.jpg" alt="JPEG" style={{height:"100px",width:"100px",borderRadius:"50%",objectFit:"cover"}}/></div>
-                    <div>Lunch Specials</div>
-                </div>} {...a11yProps(2)} style={{textTransform:"capitalize"}}/>
-                <Tab label={<div>
-                    <div><img src="https://media-cdn.grubhub.com/d_search:browse-images:default.jpg/d_search:browse-images:default.jpg/dpr_auto,c_fill,w_124,h_124,f_auto,q_auto,g_auto/search/browse-images/lunch-specials-v4.jpg" alt="JPEG" style={{height:"100px",width:"100px",borderRadius:"50%",objectFit:"cover"}}/></div>
-                    <div>Lunch Specials</div>
-                </div>} {...a11yProps(3)} style={{textTransform:"capitalize"}} />
-                <Tab label={<div>
-                    <div><img src="https://media-cdn.grubhub.com/d_search:browse-images:default.jpg/d_search:browse-images:default.jpg/dpr_auto,c_fill,w_124,h_124,f_auto,q_auto,g_auto/search/browse-images/lunch-specials-v4.jpg" alt="JPEG" style={{height:"100px",width:"100px",borderRadius:"50%",objectFit:"cover"}}/></div>
-                    <div>Lunch Specials</div>
-                </div>} {...a11yProps(4)} style={{textTransform:"capitalize"}}/>
-                <Tab label={<div>
-                    <div><img src="https://media-cdn.grubhub.com/d_search:browse-images:default.jpg/d_search:browse-images:default.jpg/dpr_auto,c_fill,w_124,h_124,f_auto,q_auto,g_auto/search/browse-images/lunch-specials-v4.jpg" alt="JPEG" style={{height:"100px",width:"100px",borderRadius:"50%",objectFit:"cover"}}/></div>
-                    <div>Lunch Specials</div>
-                </div>} {...a11yProps(5)} style={{textTransform:"capitalize"}} />
-                <Tab label={<div>
-                  <div><img src="https://media-cdn.grubhub.com/d_search:browse-images:default.jpg/d_search:browse-images:default.jpg/dpr_auto,c_fill,w_124,h_124,f_auto,q_auto,g_auto/search/browse-images/lunch-specials-v4.jpg" alt="JPEG" style={{height:"100px",width:"100px",borderRadius:"50%",objectFit:"cover"}}/></div>
-                  <div>Lunch Specials</div>
-              </div>} {...a11yProps(6)} style={{textTransform:"capitalize"}}/>
-              <Tab label={<div>
-                    <div><img src="https://media-cdn.grubhub.com/d_search:browse-images:default.jpg/d_search:browse-images:default.jpg/dpr_auto,c_fill,w_124,h_124,f_auto,q_auto,g_auto/search/browse-images/lunch-specials-v4.jpg" alt="JPEG" style={{height:"100px",width:"100px",borderRadius:"50%",objectFit:"cover"}}/></div>
-                    <div>Lunch Specials</div>
-                </div>} {...a11yProps(7)} style={{textTransform:"capitalize"}}/>
-                <Tab label={<div>
-                    <div><img src="https://media-cdn.grubhub.com/d_search:browse-images:default.jpg/d_search:browse-images:default.jpg/dpr_auto,c_fill,w_124,h_124,f_auto,q_auto,g_auto/search/browse-images/lunch-specials-v4.jpg" alt="JPEG" style={{height:"100px",width:"100px",borderRadius:"50%",objectFit:"cover"}}/></div>
-                    <div>Lunch Specials</div>
-                </div>} {...a11yProps(8)} style={{textTransform:"capitalize"}} />
-                <Tab label={<div>
->>>>>>> aa54deb2eea1f6cd34a6183b0a9ece2a146f0b88
                   <div><img src="https://media-cdn.grubhub.com/d_search:browse-images:default.jpg/d_search:browse-images:default.jpg/dpr_auto,c_fill,w_124,h_124,f_auto,q_auto,g_auto/search/browse-images/lunch-specials-v4.jpg" alt="JPEG" style={{height:"100px",width:"100px",borderRadius:"50%",objectFit:"cover"}}/></div>
                   <div>Lunch Specials</div>
               </div>} {...a11yProps(9)} style={{textTransform:"capitalize"}}/>
